cc655236f63d423e2d661ec74172ac881190f825
sasa.jotanovic98@gmail.com <sasa.jotanovic98@gmail.com>
2022-02-17 20:32:02 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  loadedFeature = 'recipe';
+  title = 'Course-project';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+}
